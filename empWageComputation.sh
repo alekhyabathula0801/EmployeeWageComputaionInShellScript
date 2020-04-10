c03a0f2ce89567788da20ac881c21bbf98df54cd
arun kumar <alekhya0801@gmail.com>
2020-04-10 19:45:15 +0530

    using function to get working hours feature added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 #constants for the program
 partTime=2
-present=1
+fullTime=1
 absent=0
 empRatePerHr=20
 numWorkingDays=20
@@ -9,19 +9,23 @@ maxWorkingHrs=100
 #variables
 totalEmpHr=0
 totalWorkingDays=0
+function getWorkingHrs(){
+    case $1 in
+        $fullTime)
+                empHr=8
+                ;;
+        $absent)
+                empHr=0
+                ;;
+        $partTime)
+                empHr=4
+                ;;
+    esac
+	echo $empHr
+}
 while [ $totalWorkingDays -lt $numWorkingDays -a $totalEmpHr -lt $maxWorkingHrs ]
 do
-    case $((RANDOM%3)) in
-	$present)
-		empHr=8
-		;;
-	$absent)
-		empHr=0
-		;;
-	$partTime)
-        	empHr=4
-		;;
-    esac
+	empHr="$( getWorkingHrs $((RANDOM%3)) )"
 	totalEmpHr=$(($totalEmpHr + $empHr ))
 	((totalWorkingDays++))
 done
