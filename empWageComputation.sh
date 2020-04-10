8307f74bc5966aa8b3f50c70fe8faa27f1fdad5b
arun kumar <alekhya0801@gmail.com>
2020-04-10 17:00:39 +0530

    compute monthly employee wage feature added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,18 +4,21 @@ partTime=2
 present=1
 absent=0
 empRatePerHr=20
-case $((RANDOM%3)) in
-$present)
-	echo employee is present
-	empHr=8
-;;
-$absent)
-	echo employee is absent
-	empHr=0
-;;
-$partTime)
-        echo employee is part time
-        empHr=4
-;;
-esac
-salary=$(($empHr*$empRatePerHr))
+numWorkingDay=20
+totalSalary=0
+for (( day=1; day<=$numWorkingDay; day++ ))
+do
+    case $((RANDOM%3)) in
+	$present)
+		empHr=8
+		;;
+	$absent)
+		empHr=0
+		;;
+	$partTime)
+        	empHr=4
+		;;
+    esac
+	salary=$(($empHr*$empRatePerHr))
+	totalSalary=$(($totalSalary + $salary ))
+done
