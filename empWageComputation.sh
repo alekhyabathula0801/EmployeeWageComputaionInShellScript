c3946ff9329afec0649bb886f9c381c9a253d043
arun kumar <alekhya0801@gmail.com>
2020-04-10 16:26:25 +0530

    compute daily employee wage feature added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,12 +1,16 @@
-#!/bin/bash
+#!/bin/bash -x
 echo Welcome to employee wage computation program
 present=1
 absent=0
+empRatePerHr=20
 case $((RANDOM%2)) in
 $present)
 	echo employee is present
+	empHr=8
 ;;
 $absent)
 	echo employee is absent
+	empHr=0
 ;;
 esac
+salary=$(($empHr*$empRatePerHr))
