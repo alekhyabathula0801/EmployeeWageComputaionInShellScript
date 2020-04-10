23e32925baff50cb5d7acbf68b70c5b24b2a2342
arun kumar <alekhya0801@gmail.com>
2020-04-10 13:05:14 +0530

    Added employee attendence feature

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 echo Welcome to employee wage computation program
+present=1
+absent=0
+case $((RANDOM%2)) in
+$present)
+	echo employee is present
+;;
+$absent)
+	echo employee is absent
+;;
+esac
